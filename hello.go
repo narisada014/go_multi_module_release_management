ba0566dac55fc1209192193b2114edfd787e32ae
narisada014 <nari.reishi@gmail.com>
2024-04-09 00:13:17 +0900

    use multi module package implement

--- hello.go
+++ hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/narisada014/go_multi_module_release_management/print_int/convert"
 	"golang.org/x/example/hello/reverse"
 )
 
 func main() {
 	fmt.Println(reverse.String("Hello"), reverse.Int(24601))
+	fmt.Println(convert.ConvertStrToInt("12345"))
 }
